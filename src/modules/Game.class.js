ef4f0aa0e30d0a908879789de60318ad1f6de47c
nikolay <diedaemon@gmail.com>
2026-02-11 22:11:00 +0100

    task complete

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,68 +1,211 @@
-'use strict';
-
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
-class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+export default class Game {
+  constructor() {
+    this.state = this.createInitialState();
+    this.score = 0;
+    this.status = 'idle';
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  createInitialState() {
+    return (this.state = Array.from({ length: 4 }, () => Array(4).fill(0)));
+  }
 
-  /**
-   * @returns {number}
-   */
-  getScore() {}
+  moveLeft() {
+    const { board: newBoard, score } = this.slideCells(this.state);
 
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
+    this.state = newBoard;
+    this.score += score;
+    this.addRandomTile();
+    this.getStatus();
 
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
-   * `idle` - the game has not started yet (the initial state);
-   * `playing` - the game is in progress;
-   * `win` - the game is won;
-   * `lose` - the game is lost
-   */
-  getStatus() {}
+    return newBoard;
+  }
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  moveRight() {
+    const reversed = this.reverse(this.state);
+    const { board: newBoard, score } = this.slideCells(reversed);
+    const resBoard = this.reverse(newBoard);
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+    this.state = resBoard;
+    this.score += score;
+    this.addRandomTile();
+    this.getStatus();
 
-  // Add your own methods here
+    return resBoard;
+  }
+
+  moveUp() {
+    const rotated = this.rotate(this.state, 'back');
+    const { board: newBoard, score } = this.slideCells(rotated);
+    const resBoard = this.rotate(newBoard);
+
+    this.state = resBoard;
+    this.score += score;
+    this.addRandomTile();
+    this.getStatus();
+
+    return resBoard;
+  }
+
+  moveDown() {
+    const rotated = this.rotate(this.state);
+    const { board: newBoard, score } = this.slideCells(rotated);
+    const resBoard = this.rotate(newBoard, 'back');
+
+    this.state = resBoard;
+    this.score += score;
+    this.addRandomTile();
+    this.getStatus();
+
+    return resBoard;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getStatus() {
+    const canMove = this.canMoveCells(this.state);
+    const isYouWin = this.state.some((row) => {
+      return row.some((cell) => cell === 2048);
+    });
+
+    if (isYouWin) {
+      this.status = 'win';
+    } else if (!canMove) {
+      this.status = 'lose';
+    } else {
+      this.status = 'playing';
+    }
+
+    return this.status;
+  }
+
+  start() {
+    this.state = this.createInitialState();
+    this.score = 0;
+    this.status = 'playing';
+
+    this.addRandomTile(this.state);
+    this.addRandomTile(this.state);
+  }
+
+  restart() {
+    this.state = this.createInitialState();
+    this.score = 0;
+
+    this.addRandomTile(this.state);
+    this.addRandomTile(this.state);
+  }
+
+  canMoveCells(board) {
+    const size = board.length;
+
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        const current = board[i][j];
+
+        if (current === 0) {
+          return true;
+        }
+
+        if (j < size - 1 && current === board[i][j + 1]) {
+          return true;
+        }
+
+        if (i < size - 1 && current === board[i + 1][j]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  getEmptyCells(board) {
+    const emptyCells = [];
+
+    board.forEach((row, i) => {
+      row.forEach((elem, j) => {
+        if (elem === 0) {
+          emptyCells.push({
+            i: i,
+            j: j,
+          });
+        }
+      });
+    });
+
+    return emptyCells;
+  }
+
+  slideCells(board) {
+    const newBoard = { board: [], score: 0 };
+
+    board.forEach((row) => {
+      const filtered = row.filter((x) => x !== 0);
+      const newRow = [];
+
+      for (let i = 0; i < filtered.length; i++) {
+        if (filtered[i] === filtered[i + 1]) {
+          const merged = filtered[i] * 2;
+
+          newRow.push(merged);
+          newBoard.score += merged;
+          i++;
+        } else {
+          newRow.push(filtered[i]);
+        }
+      }
+
+      while (newRow.length < 4) {
+        newRow.push(0);
+      }
+
+      newBoard.board.push(newRow);
+    });
+
+    return newBoard;
+  }
+
+  reverse(board) {
+    return board.map((row) => [...row].reverse());
+  }
+
+  rotate(board, direction = '') {
+    const size = board.length;
+    const newBoard = Array.from({ length: size }, () => Array(size).fill(0));
+
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (direction) {
+          newBoard[size - 1 - j][i] = board[i][j];
+        } else {
+          newBoard[j][size - 1 - i] = board[i][j];
+        }
+      }
+    }
+
+    return newBoard;
+  }
+
+  addRandomTile() {
+    const emptyCells = this.getEmptyCells(this.state);
+
+    if (!emptyCells.length) {
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    const { i, j } = emptyCells[randomIndex];
+
+    const randomValue = Math.random() < 0.9 ? 2 : 4;
+
+    this.state[i][j] = randomValue;
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,97 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class';
 
-// Write your code here
+const rows = document.querySelectorAll('.field-row');
+const score = document.querySelector('.game-score');
+const button = document.querySelector('.button');
+const messageLose = document.querySelector('.message-lose');
+const messageWin = document.querySelector('.message-win');
+const messageStart = document.querySelector('.message-start');
+
+const game = new Game();
+
+button.addEventListener('click', () => {
+  if (game.status === 'idle') {
+    game.start();
+  } else {
+    game.restart();
+  }
+
+  render(game.state);
+});
+
+document.addEventListener('keydown', (e) => {
+  if (game.status !== 'playing') {
+    return;
+  }
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+  }
+
+  render(game.state);
+});
+
+function render(board) {
+  button.classList.remove('start', 'restart');
+  messageStart.classList.add('hidden');
+  messageLose.classList.add('hidden');
+  messageWin.classList.add('hidden');
+
+  switch (game.status) {
+    case 'idle':
+      button.classList.add('start');
+      button.textContent = 'Start';
+      messageStart.classList.remove('hidden');
+      break;
+
+    case 'playing':
+      button.classList.add('restart');
+      button.textContent = 'Restart';
+      break;
+
+    case 'win':
+      button.classList.add('restart');
+      button.textContent = 'Restart';
+      messageWin.classList.remove('hidden');
+      break;
+
+    case 'lose':
+      button.classList.add('restart');
+      button.textContent = 'Restart';
+      messageLose.classList.remove('hidden');
+      break;
+  }
+
+  score.textContent = game.score;
+
+  rows.forEach((row, i) => {
+    const cells = row.querySelectorAll('.field-cell');
+
+    cells.forEach((cell, j) => {
+      const value = board[i][j];
+
+      cell.className = 'field-cell';
+      cell.textContent = value === 0 ? '' : value;
+
+      if (value !== 0) {
+        cell.classList.add(`field-cell--${value}`);
+      }
+    });
+  });
+}
